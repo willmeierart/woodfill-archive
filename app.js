0fb118d0fd906e82d1bb3b2423dcfba2c7c35422
WILL MEIER <willmeierart@gmail.com>
2017-07-03 18:25:26 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+
+const index = require('./api/index')
+const channels = require('./api/channels')
+const blocks = require('./api/blocks')
+
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', index)
+app.use('/channels', channels)
+app.use('/channels', blocks)
+
+app.use(function(req, res, next) {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+app.use(function(err, req, res, next) {
+  res.status(500)
+  res.json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  })
+})
+
+module.exports = app
